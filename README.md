06a52666089a395a546dbef26eda41cd00ec25e9
Krishna Kumar <krsnaa@Kiku-iMac-Pro.local>
2020-07-13 14:21:38 -0700

    added description

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Atlassian Facets Demonstration
+
+For this demonstration, we use the Census Income Data Set from the UCI Machine Learning Repository - http://archive.ics.uci.edu/ml/datasets/Census+Income
+
+## Facets-Overview
+
+Overview gives a high-level view of one or more data sets. It produces a visual feature-by-feature statistical analysis, and can also be used to compare statistics across two or more data sets. The tool can process both numeric and string features, including multiple instances of a number or string per feature.
+
+Overview can help uncover issues with datasets, including the following:
+
+- Unexpected feature values
+- Missing feature values for a large number of examples
+- Training/serving skew
+- Training/test/validation set skew
+
+Key aspects of the visualization are outlier detection and distribution comparison across multiple datasets. Interesting values (such as a high proportion of missing data, or very different distributions of a feature across multiple datasets) are highlighted in red. Features can be sorted by values of interest such as the number of missing values or the skew between the different datasets.
+
+![Facet Overview Demo](media/facet-overview.gif)
+
+## Facet-Dive
+
+Dive is a tool for interactively exploring up to tens of thousands of multidimensional data points, allowing users to seamlessly switch between a high-level overview and low-level details. Each example is a represented as single item in the visualization and the points can be positioned by faceting/bucketing in multiple dimensions by their feature values. Combining smooth animation and zooming with faceting and filtering, Dive makes it easy to spot patterns and outliers in complex data sets.
+
+![Facet Overview Dive](media/facet-dive.gif)
--- app.js
+++ app.js
@@ -2,32 +2,33 @@
 
 // Express is the underlying that atlassian-connect-express uses:
 // https://expressjs.com
-import express from 'express';
+import express from "express";
 
 // https://expressjs.com/en/guide/using-middleware.html
-import bodyParser from 'body-parser';
-import compression from 'compression';
-import cookieParser from 'cookie-parser';
-import errorHandler from 'errorhandler';
-import morgan from 'morgan';
+import bodyParser from "body-parser";
+import compression from "compression";
+import cookieParser from "cookie-parser";
+import errorHandler from "errorhandler";
+// import morgan from 'morgan';
+const morgan = require("morgan");
 
 // atlassian-connect-express also provides a middleware
-import ace from 'atlassian-connect-express';
+import ace from "atlassian-connect-express";
 
 // Use Handlebars as view engine:
 // https://npmjs.org/package/express-hbs
 // http://handlebarsjs.com
-import hbs from 'express-hbs';
+import hbs from "express-hbs";
 
 // We also need a few stock Node modules
-import http from 'http';
-import path from 'path';
-import os from 'os';
-import helmet from 'helmet';
-import nocache from 'nocache';
+import http from "http";
+import path from "path";
+import os from "os";
+import helmet from "helmet";
+import nocache from "nocache";
 
 // Routes live here; this is the C in MVC
-import routes from './routes';
+import routes from "./routes";
 
 // Bootstrap Express and atlassian-connect-express
 const app = express();
@@ -35,32 +36,36 @@ const addon = ace(app);
 
 // See config.json
 const port = addon.config.port();
-app.set('port', port);
+app.set("port", port);
 
 // Configure Handlebars
-const viewsDir = __dirname + '/views';
-app.engine('hbs', hbs.express4({partialsDir: viewsDir}));
-app.set('view engine', 'hbs');
-app.set('views', viewsDir);
+const viewsDir = __dirname + "/views";
+app.engine("hbs", hbs.express4({ partialsDir: viewsDir }));
+app.set("view engine", "hbs");
+app.set("views", viewsDir);
 
 // Log requests, using an appropriate formatter by env
-const devEnv = app.get('env') == 'development';
-app.use(morgan(devEnv ? 'dev' : 'combined'));
+const devEnv = app.get("env") == "development";
+app.use(morgan(devEnv ? "dev" : "combined"));
 
 // Atlassian security policy requirements
 // http://go.atlassian.com/security-requirements-for-cloud-apps
 // HSTS must be enabled with a minimum age of at least one year
-app.use(helmet.hsts({
-  maxAge: 31536000,
-  includeSubDomains: false
-}));
-app.use(helmet.referrerPolicy({
-  policy: ['origin']
-}));
+app.use(
+  helmet.hsts({
+    maxAge: 31536000,
+    includeSubDomains: false,
+  })
+);
+app.use(
+  helmet.referrerPolicy({
+    policy: ["origin"],
+  })
+);
 
 // Include request parsers
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Gzip responses when appropriate
@@ -70,7 +75,7 @@ app.use(compression());
 app.use(addon.middleware());
 
 // Mount the static files directory
-const staticDir = path.join(__dirname, 'public');
+const staticDir = path.join(__dirname, "public");
 app.use(express.static(staticDir));
 
 // Atlassian security policy requirements
@@ -85,7 +90,7 @@ routes(app, addon);
 
 // Boot the HTTP server
 http.createServer(app).listen(port, () => {
-  console.log('App server running at http://' + os.hostname() + ':' + port);
+  console.log("App server running at http://" + os.hostname() + ":" + port);
 
   // Enables auto registration/de-registration of app into a host in dev mode
   if (devEnv) addon.register();
